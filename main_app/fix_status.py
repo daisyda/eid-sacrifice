9927e784756a160baae585e32d72227fa6243cfc
Shahad <=>
2025-06-03 05:02:21 +0300

    run fix_status when need

--- main_app/fix_status.py
+++ main_app/fix_status.py
@@ -13,16 +13,15 @@ STATUS_MAP = {
 
 def fix_status_values():
     updated = 0
-    skipped = 0
 
     for obj in Udhiyah.objects.all():
         status_value = obj.status.strip()
 
+        # فقط الحالات العربية
         if status_value in STATUS_MAP:
             obj.status = STATUS_MAP[status_value]
             obj.save()
             updated += 1
-        elif status_value in STATUS_MAP.values():
-            skipped += 1
 
-    print(f"✅ Updated: {updated}, Skipped: {skipped}")
+    if updated:
+        print(f"✅ Auto-fixed {updated} status values.")
